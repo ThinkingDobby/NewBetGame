b1e8a7cf6cbaa8294e0cb01a95dca329418ac0d8
ThinkingDobby <chiho1221@naver.com>
2021-01-15 17:54:02 +0900

    First Commit

---
+++ app/src/main/java/com/thinkingdobby/newbetgame/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.thinkingdobby.newbetgame
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        screen_title.setOnClickListener {
+            val intent = Intent(this, SubActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}

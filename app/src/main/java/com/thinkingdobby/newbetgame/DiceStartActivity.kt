e69ce29d15359f43029227fc65182471316be48a
ThinkingDobby <chiho1221@naver.com>
2021-01-18 18:18:25 +0900

    2021-01-18 수정 전 임시저장

--- app/src/main/java/com/thinkingdobby/newbetgame/DiceStartActivity.kt
+++ app/src/main/java/com/thinkingdobby/newbetgame/DiceStartActivity.kt
@@ -2,11 +2,56 @@ package com.thinkingdobby.newbetgame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_coin_start.*
+import kotlin.random.Random
 
 class DiceStartActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dice_start)
+        setContentView(R.layout.activity_coin_start)
+
+        tv_coinStart_result.setText("")
+
+        val now = intent.getIntExtra("status", 0)
+        iv_coinStart_coin.setImageResource(getImage(now))
+
+        tv_coinStart_spin.setOnClickListener {
+            iv_coinStart_coin.animate().rotationX(360f).setDuration(300).start()
+            val number = getRandomValue()
+            val tmp = getImage(number)
+            iv_coinStart_coin.setImageResource(tmp)
+            tv_coinStart_result.setText("${number + 1}!")
+
+            //임시로 작성한 코드
+            tv_coinStart_spin.setText("리셋")
+
+            tv_coinStart_spin.setOnClickListener {
+                val intent = getIntent()
+                intent.putExtra("status", number)
+                finish()
+                startActivity(intent)
+                overridePendingTransition(0, 0)
+            }
+        }
+
+        tv_coinStart_back.setOnClickListener {
+            finish()
+        }
+    }
+
+    private fun getImage(value: Int): Int {
+        return when (value) {
+            0 -> R.drawable.dice_1
+            1 -> R.drawable.dice_2
+            2 -> R.drawable.dice_3
+            3 -> R.drawable.dice_4
+            4 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+    }
+
+    private fun getRandomValue(): Int {
+        return Random.nextInt(6)
     }
 }

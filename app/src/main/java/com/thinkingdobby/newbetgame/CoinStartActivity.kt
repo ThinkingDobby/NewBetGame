f4b97a49cbd057680cda2656abef93d9956199b7
ThinkingDobby <chiho1221@naver.com>
2021-01-19 18:56:29 +0900

    2021-01-19 흔들림 감지 구현 완료, 마무리 작업 시작 전

--- app/src/main/java/com/thinkingdobby/newbetgame/CoinStartActivity.kt
+++ app/src/main/java/com/thinkingdobby/newbetgame/CoinStartActivity.kt
@@ -1,11 +1,16 @@
 package com.thinkingdobby.newbetgame
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_coin_start.*
 import kotlin.random.Random
 
-class CoinStartActivity : AppCompatActivity() {
+class CoinStartActivity : AppCompatActivity(), SensorEventListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +28,7 @@ class CoinStartActivity : AppCompatActivity() {
 
         tv_coinStart_spin.setOnClickListener {
             iv_coinStart_coin.animate().rotationX(360f).setDuration(300).start()
+
             val tmp = getImage(getRandomValue())
             iv_coinStart_coin.setImageResource(tmp)
             if (checkStatus(tmp)) {
@@ -45,6 +51,75 @@ class CoinStartActivity : AppCompatActivity() {
         tv_coinStart_back.setOnClickListener {
             finish()
         }
+    }
+
+    private val sensorManager1 by lazy {
+        getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        sensorManager1.registerListener(
+            this,
+            sensorManager1.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+            SensorManager.SENSOR_DELAY_NORMAL
+        )
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+    }
+
+    private var lastUpdate = 0L
+    private val SHAKE_THRESHOLD = 600
+    private var last_x: Float = 0F
+    private var last_y: Float = 0F
+    private var last_z: Float = 0F
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (event != null) {
+            val x = event.values[0]
+            val y = event.values[1]
+            val z = event.values[2]
+
+            val curTime = System.currentTimeMillis()
+            if (tv_coinStart_spin.text == "굴리기!") {
+                if ((curTime - lastUpdate) > 100) {
+                    val diffTime = curTime - lastUpdate
+                    lastUpdate = curTime
+
+                    val speed = Math.abs(x + y + z - last_x - last_y - last_z) / diffTime * 10000
+                    if (speed > SHAKE_THRESHOLD) {
+                        iv_coinStart_coin.animate().rotationX(360f).setDuration(300).start()
+                        val tmp = getImage(getRandomValue())
+                        iv_coinStart_coin.setImageResource(tmp)
+                        if (checkStatus(tmp)) {
+                            tv_coinStart_result.setText("앞면!")
+                        } else {
+                            tv_coinStart_result.setText("뒷면!")
+                        }
+
+                        tv_coinStart_spin.setText("리셋")
+
+                        tv_coinStart_spin.setOnClickListener {
+                            val intent = getIntent()
+                            intent.putExtra("status", checkStatus(tmp))
+                            finish()
+                            startActivity(intent)
+                            overridePendingTransition(0, 0)
+                        }
+                    }
+                    last_x = x
+                    last_y = y
+                    last_z = z
+                }
+            }
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager1.unregisterListener(this)
     }
 
     private fun getImage(value: Int): Int {
--- app/src/main/java/com/thinkingdobby/newbetgame/DiceStartActivity.kt
+++ app/src/main/java/com/thinkingdobby/newbetgame/DiceStartActivity.kt
@@ -1,12 +1,17 @@
 package com.thinkingdobby.newbetgame
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.animation.AnimationUtils
 import kotlinx.android.synthetic.main.activity_coin_start.*
 import kotlin.random.Random
 
-class DiceStartActivity : AppCompatActivity() {
+class DiceStartActivity : AppCompatActivity(), SensorEventListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,6 +44,73 @@ class DiceStartActivity : AppCompatActivity() {
         tv_coinStart_back.setOnClickListener {
             finish()
         }
+    }
+
+    private val sensorManager1 by lazy {
+        getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        sensorManager1.registerListener(
+            this,
+            sensorManager1.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+            SensorManager.SENSOR_DELAY_NORMAL
+        )
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+    }
+
+    private var lastUpdate = 0L
+    private val SHAKE_THRESHOLD = 600
+    private var last_x: Float = 0F
+    private var last_y: Float = 0F
+    private var last_z: Float = 0F
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (event != null) {
+            val x = event.values[0]
+            val y = event.values[1]
+            val z = event.values[2]
+
+            val curTime = System.currentTimeMillis()
+            if (tv_coinStart_spin.text == "굴리기!") {
+                if ((curTime - lastUpdate) > 100) {
+                    val diffTime = curTime - lastUpdate
+                    lastUpdate = curTime
+
+                    val speed = Math.abs(x + y + z - last_x - last_y - last_z) / diffTime * 10000
+                    if (speed > SHAKE_THRESHOLD) {
+                        val shake = AnimationUtils.loadAnimation(this, R.anim.shake)
+                        iv_coinStart_coin.startAnimation(shake)
+                        val number = getRandomValue()
+                        val tmp = getImage(number)
+                        iv_coinStart_coin.setImageResource(tmp)
+                        tv_coinStart_result.setText("${number + 1}!")
+
+                        tv_coinStart_spin.setText("리셋")
+
+                        tv_coinStart_spin.setOnClickListener {
+                            val intent = getIntent()
+                            intent.putExtra("status", number)
+                            finish()
+                            startActivity(intent)
+                            overridePendingTransition(0, 0)
+                        }
+                    }
+                    last_x = x
+                    last_y = y
+                    last_z = z
+                }
+            }
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager1.unregisterListener(this)
     }
 
     private fun getImage(value: Int): Int {
